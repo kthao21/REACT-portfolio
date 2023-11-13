93c17b5db16516811c416e6f35a260620c2dd12d
Kalia Thao <kaliathao21@gmail.com>
2023-11-12 21:21:15 -0800

    Adding projects to portfolio

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,13 +1,16 @@
 import { Link, useLocation } from 'react-router-dom';
 
-function Project() {
-    const currentPage = useLocation().pathname;
-    
+const Project = () => {
     return (
-        <section>
-
-        </section>
-    )
-}
+      <div>
+        <a href="https://morning-spire-89765-733c4b62d1d7.herokuapp.com/" target="_blank" rel="noreferrer">
+          <img src="chatea.png" alt="ChaTea" />
+        </a>
+        <a href="https://kthao21.github.io/Vibrant_Vocab/" target="_blank" rel="noreferrer">
+          <img src="vibrantvocabs.png" alt="Vibrant Vocabs" />
+        </a>
+      </div>
+    );
+  };
 
 export default Project;
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,9 +2,27 @@ export default function Resume() {
   return (
     <div>
       <h2>Resume</h2>
-      <p>
-
-      </p>
+        <h3>Experience</h3>
+          <ul>
+            <li>Receptionist/Dental Assistant - Elk Grove, CA: Jan 2022 - Present</li>
+            <li>Licensed Dental Assistant at Park Dental - St. Paul, MN: Oct 2020 - Oct 2021 </li>
+            <li>Brand Operations Manager at Victoria's Secret - Bloomington, MN: March 2016 - Nov 2020</li>
+          </ul>
+        <h3>Education</h3>
+          <ul>
+            <li>Century College - White Bear Lake, MN: Associate of Applied Science Degree Dental Assistant - 2018</li>
+            <li>Century College - White Bear Lake, MN: Associate in Arts Degree - 2016</li>
+            <li>Park High School - Cottage Grove, MN: High School Diploma - 2014</li>
+          </ul>
+        <h3>Skills</h3>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>MySQL</li>
+          <li>NodeJS</li>
+          <li>Team Player</li>
+        </ul>
     </div>
   );
 }
